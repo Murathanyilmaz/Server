dfa0c32d2175e2de41d9bb8dd4531ce97cd3d3b8
Murathan <97229162+Murathanyilmaz@users.noreply.github.com>
2025-12-08 01:55:52 +0000

    CORS

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use(cors({
+  origin: "https://murathanyilmaz.net"
+}));
+
 app.get("/", (req, res) => {
   res.send("Server working!");
 });
