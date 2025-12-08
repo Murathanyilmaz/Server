1a643ad2ad3c86bc62bd1dea8b122eefe0e122d6
Murathan <97229162+Murathanyilmaz@users.noreply.github.com>
2025-12-08 01:39:11 +0000

    added Render port

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 app.get("/", (req, res) => {
   res.send("Server working!");
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
